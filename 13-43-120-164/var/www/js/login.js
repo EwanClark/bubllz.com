6f96b6b58c7b44d2a6b13adf7dc0ae3e28e81a8b
EwanClark <ewanclark4312@gmail.com>
2024-12-21 19:22:25 +0000

    fix: improve login error handling and streamline fetch logic

--- 13-43-120-164/var/www/js/login.js
+++ 13-43-120-164/var/www/js/login.js
@@ -1,46 +1,40 @@
-var login = true;
-
 function LoginButtonClicked() {
     var usernameInputElement = document.getElementById("Username");
     var usernameInputted = usernameInputElement.value;
     var passwordInputElement = document.getElementById("Password");
     var passwordInputted = passwordInputElement.value;
     if (usernameInputted == "" || passwordInputted == "") {
-        alert("Please fill out all fields");
-        throw new Error('Please fill out all fields.');
-}
-else {
-    fetch("https://bubllz.com/api/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            "Username": usernameInputted,
-            "Password": passwordInputted
+        return alert("Please fill out all fields");
+    } else {
+        var login = true;
+        fetch("https://bubllz.com/api/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "Username": usernameInputted,
+                "Password": passwordInputted
+            })
         })
-    })
-    .then(response => {
-        if (response.status === 401) {
-            login = false;
-            alert("Invalid username or password");
-        }
-        return response.json()
-    })
-    .then(data => {
-        if (!login) {
-            return;
-        }
-        else{
-            console.log('Success, token stored!');
-            localStorage.setItem('token', data.message);
-            alert(`User logged in successfully! as ${usernameInputted}`);
-            window.location.href = 'https://bubllz.com';
-        }
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-        alert(`Error logging in, ${error}`);
-    });
-}
+        .then(response => {
+            if (response.status === 401) {
+                login = false;
+                alert("Invalid username or password");
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (login) {
+                console.log('Success, token stored!');
+                localStorage.setItem('token', data.message);
+                alert(`User logged in successfully! as ${usernameInputted}`);
+                window.location.href = 'https://bubllz.com';
+            }
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+            alert(`Error logging in, ${error}`);
+        });
+    }
 }
