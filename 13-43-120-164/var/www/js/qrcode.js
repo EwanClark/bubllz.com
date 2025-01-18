2da8488bbd8f388a5076571fb92e04d587d25b86
EwanClark <ewanclark4312@gmail.com>
2025-01-18 21:23:29 +0000

    qrcode added

---
+++ 13-43-120-164/var/www/js/qrcode.js
@@ -0,0 +1,62 @@
+function stylishAlert(message) {
+    // Set the message in the modal
+    document.getElementById('alert-message').textContent = message;
+
+    // Show the modal
+    document.getElementById('custom-alert').style.display = 'flex';
+
+    // Close the modal when clicking the close button or OK button
+    document.getElementById('close-alert').onclick = closeModal;
+    document.getElementById('alert-ok').onclick = closeModal;
+}
+
+function closeModal() {
+    document.getElementById('custom-alert').style.display = 'none';
+}
+
+
+
+document.getElementById('generate-btn').addEventListener('click', function () {
+    const data = document.getElementById('data').value;
+    if (!data) {
+        return stylishAlert('Please enter some text to generate QR code.');
+    }
+
+    const qrDisplay = document.getElementById("qr-display");
+    qrDisplay.innerHTML = "";
+
+    const customWidth = 450;
+    const customHeight = 450;
+
+    const canvas = document.createElement("canvas");
+    
+    canvas.width = customWidth;
+    canvas.height = customHeight;
+    
+    qrDisplay.appendChild(canvas);  // Add the canvas to the display
+
+    // Generate QR code using QRCode.toCanvas()
+    QRCode.toCanvas(canvas, data, { width: customWidth, height: customHeight}, function (error) {
+        if (error) {
+            stylishAlert(error)
+            document.getElementById('download-btn').disabled = true;
+            qrDisplay.innerHTML = `<p>Generated QR code will appear here.</p>`;
+        } else {
+            document.getElementById('download-btn').disabled = false;
+        }
+    });
+});
+document.getElementById('download-btn').disabled = true;
+
+document.getElementById('download-btn').addEventListener('click', function () {
+    const qrCanvas = document.querySelector("#qr-display canvas"); // Get the canvas element
+    if (qrCanvas) {
+        const dataURL = qrCanvas.toDataURL("image/png"); // Convert canvas to image
+        const link = document.createElement("a");
+        link.href = dataURL;
+        link.download = "qrcode.png";
+        link.click();
+    } else {
+        stylishAlert("Please generate a QR code first.");
+    }
+});
